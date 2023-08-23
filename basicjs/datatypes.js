b3c7ac04f0c872f8a71ca7ab5afb071d2e825716
Sujay sahu <107695887+Sujay02sahu@users.noreply.github.com>
2023-08-23 07:30:32 +0000

    about of js datatypes

---
+++ basicjs/datatypes.js
@@ -0,0 +1,41 @@
+"use strict"; // *treat all js code as newer version
+
+// alert(3+3) // *we are using nodejs, not browser
+
+// console.log("hello") console.log("world") // *wrong way...X
+
+// console.log("hello"); console.log("world") // *wrong way..._/
+
+console.log("hello")
+console.log("world") // *right way code readbility should be high
+
+
+// Now the type of datatype :-
+
+// number => 2 to power 53
+// bigint => maximum length of number
+// string  => "use only"
+// boolean => true/false | yes/no | login/logout
+// null => standalone value
+// undefined => uninitialized value
+// symbol => unique
+
+// object => use of class
+
+
+const id = 77262788677678769;
+let enroll = 221;
+let Mname = "Utechh";
+let loggedIn = true;
+let scolership = null;
+let semester;
+
+console.log(typeof(7.9))
+console.log(typeof id)
+console.log(typeof enroll)
+console.log(typeof Mname)
+console.log(typeof loggedIn)
+console.log(typeof scolership) // null = object
+console.log(typeof semester) // undefined = undefined
+console.log(typeof undefined)
+console.log(typeof null)
